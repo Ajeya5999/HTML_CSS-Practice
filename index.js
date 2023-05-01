abe28b2ec620d24a1269e4afa28cbbec4cb71daa
Ajeya <ajeyasengar@gmail.com>
2023-05-01 18:19:52 +0530

    tenth commit: trying to add rotation functionality, needs work

---
+++ index.js
@@ -0,0 +1,35 @@
+// Getting elements on the HTML page
+
+    // Getting Playlist Carousel Elements
+
+const btnLeft = document.getElementById("rotate-left");
+const btnRight = document.getElementById("rotate-right");
+const playlistCarousel = document.getElementById("playlist-carousel");
+var rotationValue = 0;
+
+// Adding Event Listeners
+
+    // Playlist Carousel Evenet Listeners
+
+btnLeft.addEventListener('click', function(){
+    handlebtn(true);
+});
+
+btnRight.addEventListener('click', function(){
+    handlebtn(false);
+})
+
+//Functions
+
+    //Playlsit Carousel Functions
+
+function handlebtn(isLeft){  
+    console.log("The value is " + rotationValue);
+    if(isLeft){
+        rotationValue -= 60;
+    }
+    else{
+        rotationValue += 60;
+    }
+    playlistCarousel.style.setProperty("--playlist-carousel-rotation", String(rotationValue));
+}
